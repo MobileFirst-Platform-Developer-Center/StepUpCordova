2cba1c3eac959742d570de911f20cfd0b42e3354
Nathan Hazout <nathanh@il.ibm.com>
2016-03-15 14:22:46 +0200

    Basic UI

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,18 @@
+/**
+* Copyright 2016 IBM Corp.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 var Messages = {
     // Add here your messages for the default language.
     // Generate a similar file with a language suffix containing the translated messages.
@@ -11,40 +26,44 @@ var wlInitOptions = {
 
 // Called automatically after MFP framework initialization by WL.Client.init(wlInitOptions).
 function wlCommonInit(){
-	// Common initialization code goes here
-    document.getElementById('app_version').textContent = WL.Client.getAppProperty("APP_VERSION");
-    document.getElementById('mobilefirst').setAttribute('style', 'display:block;');
+    document.getElementById("getBalance").addEventListener("click", getBalance);
+    //var userLoginChallengeHandler = UserLoginChallengeHandler();
+
+    // WLAuthorizationManager.obtainAccessToken(userLoginChallengeHandler.securityCheckName).then(
+    //     function (accessToken) {
+    //         WL.Logger.debug("obtainAccessToken onSuccess");
+    //         showProtectedDiv();
+    //     },
+    //     function (response) {
+    //         WL.Logger.debug("obtainAccessToken onFailure: " + JSON.stringify(response));
+    //         showLoginDiv();
+    // });
+    showProtectedDiv();
 }
 
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
+function showLoginDiv() {
+    document.getElementById('protectedDiv').style.display = 'none';
+    document.getElementById('statusMsg').innerHTML = "";
+    document.getElementById('loginDiv').style.display = 'block';
+    document.getElementById('logout').style.display = 'none';
+}
 
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
+function showProtectedDiv() {
+    document.getElementById('loginDiv').style.display = 'none';
+    document.getElementById('resultLabel').innerHTML = "";
+    document.getElementById('protectedDiv').style.display = 'block';
+    document.getElementById('logout').style.display = 'block';
+}
 
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, 'app.receivedEvent(...);' must be explicitly called.
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-
-    // Update the DOM on a received event.
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
-
-app.initialize();
+function getBalance () {
+    var resourceRequest = new WLResourceRequest("/adapters/ResourceAdapter/balance", WLResourceRequest.GET);
+    resourceRequest.send().then(
+        function (response) {
+            WL.Logger.debug("Balance: " + response.responseText);
+            document.getElementById("resultLabel").innerHTML = "Balance: " + response.responseText;
+        },
+        function (response) {
+            WL.Logger.debug("Failed to get balance: " + JSON.stringify(response));
+            document.getElementById("resultLabel").innerHTML = "Failed to get balance.";
+        });
+}
